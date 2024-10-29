6aa13d2a54073034dbc0d292fb6f424ebb70340b
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2024-10-29 18:22:34 +0100

    add analysis DomainLenght

--- 1.preprocessing.R
+++ 1.preprocessing.R
@@ -39,14 +39,6 @@ moda <- function(x) {
   ux[which.max(tabulate(match(x, ux)))]
 }
 
-# skewness function
-skw <- function(x) {
-  n <- length(x)
-  m2 <- (n-1)*var(x)/n 
-  m3 <- (sum((x - mean(x))^3))/n
-  m3 / (m2^1.5)
-}
-
 # ------------------------------------------------------------------------------
 # ATTRIBUTE 'label'
 
@@ -150,6 +142,8 @@ sd(df$URLLength)
 # distribution form
 skw_value <- skewness(df$URLLength)       # (gamma > 0) right skewed
 kurtosis_value <- kurtosis(df$URLLength)  # leptokurtic
+skw_value
+kurtosis_value
 
 n <- length(df$URLLength)
 h_sturges <- (max(df$URLLength) - min(df$URLLength)) / sqrt(n)
@@ -161,9 +155,77 @@ legend("topright", legend = c(paste("Skewness:", round(skw_value, 2)),
                               paste("Kurtosis:", round(kurtosis_value, 2))), 
        bty = "n", col = "black", cex = 0.8)
 
+# TODO: distribution form for 0 and 1
 # ------------------------------------------------------------------------------
 # ATTRIBUTE 'DomainLenght'
 
-# ------------------------------------------------------------------------------
+summary(df$DomainLength)
 
+breaks <- c(0, 10, 20, 30, 40, 50, 100) 
+
+j_freq <- table(df$label, cut(df$DomainLength, breaks = breaks))
+j_freq_rel <- prop.table(j_freq)
+
+barplot(j_freq_rel, col = c("orange", "lightblue"),
+        legend = c("phishing", "legitimate"),
+        main = "Frequenza relativa congiunta DomainLength")
+
+# outliers
+df_0 <- df[df$label == 0, ]
+df_1 <- df[df$label == 1, ]
+
+summary(df_0$DomainLength)
+summary(df_1$DomainLength)
+
+boxplot(df_0$DomainLength, df_1$DomainLength,
+        main = 'Boxplot DomainLength', col = c('orange', 'lightblue'),
+        names = c('phishing', 'legitimate'))
+
+# overlap median
+
+IQR_0 <- quantile(df_0$DomainLength, 0.75) - quantile(df_0$DomainLength, 0.25)
+# lower bound
+M1_0 <- quantile(df_0$DomainLength, 0.5) - 1.57*IQR_0/sqrt(length(df_0$DomainLength))
+# upper bound
+M2_0 <- quantile(df_0$DomainLength, 0.5) + 1.57*IQR_0/sqrt(length(df_0$DomainLength))
+
+IQR_1 <- quantile(df_1$DomainLength, 0.75) - quantile(df_1$DomainLength, 0.25)
+# lower bound
+M1_1 <- quantile(df_1$DomainLength, 0.5) - 1.57*IQR_1/sqrt(length(df_1$DomainLength))
+# upper bound
+M2_1 <- quantile(df_1$DomainLength, 0.5) + 1.57*IQR_1/sqrt(length(df_1$DomainLength))
+
+# no overlap: the median is different with a level signification of 5%
+c(M1_0, M2_0)
+c(M1_1, M2_1)
+
+# dispersion
+var(df$DomainLength)
+sd(df$DomainLength)
+
+# distribution form
+skw_value <- skewness(df$DomainLength)       # (gamma > 0) right skewed
+kurtosis_value <- kurtosis(df$DomainLength)  # leptokurtic
+skw_value
+kurtosis_value
+
+n <- length(df$DomainLength)
+h_sturges <- (max(df$DomainLength) - min(df$DomainLength)) / sqrt(n)
+density_sturges <- density(df$DomainLength, bw = h_sturges)
+
+plot(density_sturges, main = "Forma della distribuzione DomainLength", 
+     col = "orange", lwd = 2)
+legend("topright", legend = c(paste("Skewness:", round(skw_value, 2)), 
+                              paste("Kurtosis:", round(kurtosis_value, 2))), 
+       bty = "n", col = "black", cex = 0.8)
+
+# TODO: distribution form for 0 and 1
+# ------------------------------------------------------------------------------
+# ATTRIBUTE 'IsDomainIP'
+
+table(df$IsDomainIP)
+
+# delete attribute because have only 81 value setting to 1
+df <- subset(df, select = -IsDomainIP)
+# ------------------------------------------------------------------------------
 # correlations
